7aff1e4034384eb66c67634dc698c5c8a9791981
TobiasLeuthel <tobias.leuthel@gmail.com>
2024-10-24 10:39:22 +0200

    Testen Zusammenarbeit

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Test")
+import time
+import numpy
+
+# Aktuelle Zeit in Sekunden seit dem Epoch
+current_time = time.time()
+print(f"Aktuelle Zeit (in Sekunden seit 1. Januar 1970): {current_time}")
+
+import numpy as np
+
+# 1. Ein einfaches Array erstellen
+array = np.array([1, 2, 3, 4, 5])
+print("Array:", array)
